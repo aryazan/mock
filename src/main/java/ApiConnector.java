0d7d670146535516fa21b27f6c09f3bcf4a89be5
a.ryazantsev <artsiom.rasantsau@gmail.com>
2018-08-05 17:34:04 +0300

    initial commit

---
+++ src/main/java/ApiConnector.java
@@ -0,0 +1,50 @@
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+import java.io.IOException;
+
+public class ApiConnector {
+
+    private static ApiConnector instance;
+    private HttpClient client;
+    private GetMethod get;
+    private PostMethod post;
+
+    public static ApiConnector getInstance(){
+        if(instance == null){
+            instance = new ApiConnector();
+        }
+        return instance;
+    }
+
+    private ApiConnector(){
+        this.get = new GetMethod("https://httpbin.org/get");
+        this.post = new PostMethod("https://httpbin.org/post");
+    }
+
+    public int performGetRequest(String textMessage){
+        System.out.println("Send request");
+        int status = 0;
+        try {
+            client = new HttpClient();
+            status = get.getStatusCode();
+            client.executeMethod(get);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(textMessage);
+        return status;
+    }
+
+    public void performPostRequest(String textMessage, String paramName, String paramValue){
+        post.addParameter(paramName, paramValue);
+        System.out.println(textMessage);
+        try {
+            client = new HttpClient();
+            client.executeMethod(post);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Application.java
@@ -0,0 +1,47 @@
+import org.apache.http.HttpStatus;
+
+public class Application {
+    private void sendGetApiRequestViaConnector(){
+        ApiConnector.getInstance().performGetRequest("perform GET request");
+    }
+
+    private void sendPostRequestViaConnector(){
+        ApiConnector.getInstance().performPostRequest("perform POST request", "paramName", "paramValue");
+    }
+
+    private void runGetRequestsInAnotherThread(){
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                ApiConnector.getInstance().performGetRequest("perform GET request in thread" + Thread.currentThread().getId());
+            }
+        });
+        t.start();
+    }
+
+    private void runPostIfGet200() throws Exception {
+        int status = ApiConnector.getInstance().performGetRequest("text");
+        if(status == HttpStatus.SC_OK){
+            ApiConnector.getInstance().performPostRequest("text", "param", "value");
+        } else {
+            throw new Exception("GET response is not 200");
+        }
+    }
+
+    public void runMethod(int method) throws Exception {
+        switch (method){
+            case 1:
+                sendGetApiRequestViaConnector();
+                break;
+            case 2:
+                sendPostRequestViaConnector();
+                break;
+            case 3:
+                runGetRequestsInAnotherThread();
+                break;
+            case 4:
+                runPostIfGet200();
+                break;
+            default: break;
+        }
+    }
+}
